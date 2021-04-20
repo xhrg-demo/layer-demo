747ef5d233f76f80667e8a5e2660084437ce2cfa
xhrg <634789257@qq.com>
2021-04-20 10:39:59 +0800

    1

---
+++ java-layer-demo/src/main/java/com/github/xhrg/layer/JavaLayerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLayerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLayerDemoApplication.class, args);
+	}
+
+}
---
+++ java-layer-demo/src/test/java/com/github/xhrg/layer/JavaLayerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.layer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLayerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
