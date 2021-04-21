c020a06f5c731559bdd57dd8458bfd767d383634
xhrg <634789257@qq.com>
2021-04-21 18:27:42 +0800

    代码优化

--- java-layer-demo/demo-api/src/main/java/com/github/xhrg/demo/api/http/HttpOrderAction.java
+++ java-layer-demo/demo-api/src/main/java/com/github/xhrg/demo/api/http/HttpOrderAction.java
@@ -1,5 +1,14 @@
 package com.github.xhrg.demo.api.http;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/order")
+@RestController
 public class HttpOrderAction {
 
+    @RequestMapping("/query")
+    public Object query() {
+        return "query";
+    }
 }
--- java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
+++ java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
@@ -11,3 +11,5 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
+
