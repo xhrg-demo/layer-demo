5ad133115fa20c2ac4473dfae6500526a379356b
xhrg <634789257@qq.com>
2021-04-21 14:30:36 +0800

    add DemoApplication

--- java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
+++ java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
@@ -1,13 +1,13 @@
 package com.github.xhrg.demo;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//springboot的DemoApplication所在包路径，必须是所有子项目包路径的父路径,否则扫描不到注解
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-
-
-
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
