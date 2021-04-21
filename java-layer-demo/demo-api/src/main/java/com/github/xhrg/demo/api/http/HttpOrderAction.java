f88093ca652fbb25ed97e745acb138b2acbc2a3c
xhrg <634789257@qq.com>
2021-04-21 19:22:56 +0800

    add logj4j

--- java-layer-demo/demo-api/src/main/java/com/github/xhrg/demo/api/http/HttpOrderAction.java
+++ java-layer-demo/demo-api/src/main/java/com/github/xhrg/demo/api/http/HttpOrderAction.java
@@ -1,6 +1,8 @@
 package com.github.xhrg.demo.api.http;
 
 import com.github.xhrg.demo.app.facade.OrderFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpOrderAction {
 
+    private final static Logger log = LoggerFactory.getLogger(HttpOrderAction.class);
+
     @Autowired
     private OrderFacade orderFacade;
 
     @RequestMapping("/query")
     public Object query() {
+        log.info("order/query");
         return orderFacade.query();
     }
 }
--- java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
+++ java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.github.xhrg.demo;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //springboot的DemoApplication所在包路径，必须是所有子项目包路径的父路径,否则扫描不到注解
 @SpringBootApplication
 public class DemoApplication {
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
