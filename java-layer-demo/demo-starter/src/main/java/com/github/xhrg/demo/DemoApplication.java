377a0f385f47739ca1f1c284c8117313beaf237d
xhrg <634789257@qq.com>
2021-04-21 14:27:12 +0800

    修改pom文件

---
+++ java-layer-demo/demo-starter/src/main/java/com/github/xhrg/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.demo;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+
+
+
+    }
+}
